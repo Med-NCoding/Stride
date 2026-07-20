54b93924123b5ecef32d63b704002b1106d6e511
Medhansh <medhanshnegi4@gmail.com>
2026-07-20 03:22:54 -0400

    Initial Commit

---
+++ Stride/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stride
+//
+//  Created by Medhansh Negi on 2026-07-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stride/StrideApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrideApp.swift
+//  Stride
+//
+//  Created by Medhansh Negi on 2026-07-20.
+//
+
+import SwiftUI
+
+@main
+struct StrideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
